e216ada35e91264dd744e98c61506474f546af12
Alejandra Vovides <geohintonia@gmail.com>
2020-07-17 10:19:50 +0100

    Add files via upload
    Script to reproduce Supplementary Information Figure 1 and Supplementary information Figure 2

---
+++ figures/script_FigS1S2_suppl.R
@@ -0,0 +1,125 @@
+# Required packages
+library(ggplot2)
+library(ggpubr)
+library(signs)
+
+#### Load data ####
+#load(file = "./data/data_asymNei.rda")
+load("~/Documents/Offis backups March 2020/NatureMSRevission/Nature_MS_Revised/GRINanalysis-master/data/data_asymNei.rda") # LM.trees
+
+###### Supplementary Information Figures ####
+
+SI_Fig1A<-ggplot(data = d4, aes(x = Hegyi, y = DBH, color = Condition,size=Hegyi)) +
+   geom_point( alpha=0.5) +
+   xlab("Stem diameter (cm)") +
+   ylab("Tree height (m)")+
+   scale_x_continuous(expression(paste( ~italic(CI))),limits = c(0, 30))+
+   scale_y_continuous(expression(paste("Stem diameter", " (cm)")), limits = c(0, 60))+
+   scale_color_manual(name="Condition",
+                      values=c("#008080","#800080"),
+                      breaks= c("grafted", "non-grafted"),
+                      labels=c("Grafted", "Non-grafted"))+
+   scale_size(expression(paste( ~italic(CI))),
+              breaks = c(0, 10, 20, 30,40))+
+   #labels = expression(15^2, 17^2, 19^2, 21^2))
+   theme(panel.grid = element_blank(),
+         panel.background = element_blank(),
+         axis.text.x = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),size=12, colour = "black" ),
+         axis.text.y = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),size=12, colour = "black"),
+         axis.title.x = element_text(size=14, colour = "black"),
+         axis.title.y = element_text(size=14, colour = "black"),
+         axis.ticks.length=unit(-1.5, "mm"),
+         axis.ticks = element_line(size = .5),
+         axis.line = element_line(colour = 'black', size = 1),
+         legend.background = element_rect(fill = NA),
+         legend.key = element_rect(fill = NA, color = NA))+
+   
+   guides(fill = guide_legend(keywidth = 1, keyheight = 1),
+          linetype=guide_legend(keywidth = 2.5, keyheight = 1),
+          colour=guide_legend(keywidth = 2.5, keyheight = 2),
+          size = guide_legend(ncol = 2, byrow = F))
+
+
+SI_Fig1B<-ggplot(data = d4, aes(x =DBH, y = height, color = Condition,size=Hegyi)) +
+   geom_point(alpha=0.5) +
+   xlab("Stem diameter (cm)") +
+   ylab("Tree height (m)")+
+   scale_color_manual(name="Condition",
+                      values=c("#008080","#800080"),
+                      breaks= c("grafted", "non-grafted"),
+                      labels=c("Grafted", "Non-grafted"))+
+   theme(panel.grid = element_blank(),
+         panel.background = element_blank(),
+         axis.text.x = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),size=12, colour = "black" ),
+         axis.text.y = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),size=12, colour = "black"),
+         axis.title.x = element_text(size=14, colour = "black"),
+         axis.title.y = element_text(size=14, colour = "black"),
+         axis.ticks.length=unit(-1.5, "mm"),
+         axis.ticks = element_line(size = .5),
+         axis.line = element_line(colour = 'black', size = 1),
+         legend.background = element_rect(fill = NA),
+         legend.key = element_rect(fill = NA, color = NA))+
+   
+   guides(fill = guide_legend(keywidth = 1, keyheight = 1),
+          linetype=guide_legend(keywidth = 2.5, keyheight = 1),
+          colour=guide_legend(keywidth = 2.5, keyheight = 2),
+          size = guide_legend(ncol = 2, byrow = F))
+
+#tiff("Supp_Info_Fig1.tiff", height = 1500, width = 2000, res=300)
+ggarrange(SI_Fig1A, SI_Fig1B,common.legend = TRUE, labels= c("A","B"))
+#dev.off()
+
+SI_Fig2A<-ggplot(d4, aes(x = AsymmNeighDBH, y = DBH, colour=Condition)) + #shift between d2 (all trees) and d5, Trees with asymmetry
+   geom_point(aes(size=AsymmNeighDBH), alpha=0.5)+
+   xlab("Neighbourhood asymmetry index") +
+   ylab("Stem diameter (cm)") +
+   scale_size_continuous(name=expression(atop(bold("Neighbourhood"), paste(bold("asymmetry")))))+
+   scale_color_manual(name="Condition",
+                      values=c("#008080","#800080"),
+                      breaks= c("grafted", "non-grafted"),
+                      labels=c("Grafted", "Non-Grafted"))+
+   theme(panel.grid = element_blank(),
+         panel.background = element_blank(),
+         axis.text.x = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),size=12, colour = "black" ),
+         axis.text.y = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),size=12, colour = "black"),
+         axis.title.x = element_text(size=14, colour = "black"),
+         axis.title.y = element_text(size=14, colour = "black"),
+         axis.ticks.length=unit(-1.5, "mm"),
+         axis.ticks = element_line(size = .5),
+         axis.line = element_line(colour = 'black', size = 1),
+         legend.background = element_rect(fill = NA),
+         legend.key = element_rect(fill = NA, color = NA))+
+   guides(size = guide_legend(nrow = 2, byrow = T)) 
+
+SI_Fig2B<-ggplot(data = d4, aes(x =DBH, y = height, color = Condition,size=AsymmNeighDBH)) +
+   geom_point(alpha=0.5) +
+   xlab("Stem diameter (cm)") +
+   ylab("Tree height (m)")+
+   scale_size_continuous(name=expression(atop(bold("Neighbourhood"), paste(bold("asymmetry")))))+
+   scale_color_manual(name="Condition",
+                      values=c("#008080","#800080"),
+                      breaks= c("grafted", "non-grafted"),
+                      labels=c("Grafted", "Non-grafted"))+
+   theme(panel.grid = element_blank(),
+         panel.background = element_blank(),
+         axis.text.x = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),size=12, colour = "black" ),
+         axis.text.y = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),size=12, colour = "black"),
+         axis.title.x = element_text(size=14, colour = "black"),
+         axis.title.y = element_text(size=14, colour = "black"),
+         axis.ticks.length=unit(-1.5, "mm"),
+         axis.ticks = element_line(size = .5),
+         axis.line = element_line(colour = 'black', size = 1),
+         legend.background = element_rect(fill = NA),
+         legend.key = element_rect(fill = NA, color = NA))+
+   
+   guides(fill = guide_legend(keywidth = 1, keyheight = 1),
+          linetype=guide_legend(keywidth = 2.5, keyheight = 1),
+          colour=guide_legend(keywidth = 2.5, keyheight = 2),
+          size = guide_legend(ncol = 2, byrow = F))
+
+
+Sup_Info_Figure2<-ggarrange(SI_Fig2A,SI_Fig2B, common.legend = TRUE, labels = c("A","B"),widths = 1, heights = 1)
+#tiff("NeighAsymm_dbh.tiff", width=2000, height=2000, res=300)
+Sup_Info_Figure2
+#dev.off()
+
