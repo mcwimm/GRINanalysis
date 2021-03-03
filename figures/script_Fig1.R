7d5fa378329ae008e19b1adfff54e7e67fcc604b
marie <mcwimm@bitbucket.org>
2021-03-03 13:18:01 +0100

    Add figure 1 script and La Mancha map

--- figures/script_Fig1.R
+++ figures/script_Fig1.R
@@ -1,57 +1,78 @@
 # required packages
 library(tidyverse)
-library(BBmisc)
-library(ggpubr)
+library(ggpubr)  # ggarrange
 library(ggforce) # geom_circle
+library(png)
 
 
-# required data
+# Import the La Mancha map (Fig 1 A)
+img <- readPNG("figures/Map2.png")
+P3 <- ggplot() + 
+   theme_void()+
+   background_image(img)
+
+# Top view of selected study sites (Fig 1 B)
+# required La Mancha data
 load("./data/LMtrees.Rda") 
 load("./data/LMlinks.Rda") 
-
-
-# subset data
 # low stress = LOC 1
 # mid stress = LOC 9
 # high stress = LOC 12
-trees = LM.trees[LM.trees$LOC == 1, ] # 9, 12
-links = LM.links[LM.links$LOC == 1, ] # 9, 12
 
-# add alpha value for visualization of grafted and non-grafted trees
-trees$alpha <- ifelse(trees$groupBin == "group", 0.5, 0.2)
-
-# set LOC-variable as factor
-links$LOC = factor(links$LOC)
-
-# create figure
-x11()
-ggplot() + 
+l = LM.links %>% 
+   filter(LOC %in% c(1, 9, 12)) %>% 
+   mutate(LOC = factor(LOC))
+t = LM.trees %>% 
+   filter(LOC %in% c(1, 9, 12)) %>% 
+   mutate(alpha = ifelse(groupBin == "group", 0.5, 0.2))
+Fig1B = t %>% 
+   ggplot(.) + 
    coord_fixed() +
-   geom_circle(trees, mapping = aes(x0=x, y0=y, r = CR, 
-                                    alpha = alpha, fill = Sp), size=0.1) +
-   geom_segment(links, mapping = aes(x=x1, y=y1, xend=x2, yend=y2), size = 0.8) +
-   geom_point(trees[trees$netDeg != 0, ], mapping=aes(x=x, y=y),
+   geom_circle(mapping = aes(x0=x, y0=y, r = CR, 
+                             alpha = alpha, fill = Sp, color=NA),
+               size=0.1) +
+   geom_segment(l, mapping = aes(x=x1, y=y1, xend=x2, yend=y2), 
+                size = 0.8) +
+   geom_point(t[t$netDeg != 0, ], mapping=aes(x=x, y=y),
               shape = 16, col = "khaki1", alpha = 0.9, size = 0.8) +
-   
+   facet_wrap(~ LOC, ncol = 1) +
    scale_alpha_continuous(range = c(0.2, 0.5),
                           guide = "none") +
    scale_fill_manual(values = c("chartreuse4", "#56B4E9", "#E69F00"),
-                     labels = c("Avicennia germinans", "Laguncularia racemosa",
-                                "Rhizophora mangle"),
+                     labels = c("A. germinans", "L. racemosa",
+                                "R. mangle"),
                      name = "Species",
                      guide = guide_legend(
-                        label.theme = element_text(face = "italic", size = 10),
-                        title.theme = element_text(face = "bold", size = 10))) +   
+                        label.theme = element_text(face = "italic", 
+                                                   size = 14),
+                        title.theme = element_text(face = "bold", 
+                                                   size = 14))) +
+   scale_color_manual(values = c("chartreuse4", "#56B4E9", "#E69F00"),
+                      labels = c("A. germinans", "L. racemosa",
+                                 "R. mangle"),
+                      name = "Species",
+                      guide = guide_legend(
+                         label.theme = element_text(face = "italic", 
+                                                    size = 20),
+                         title.theme = element_text(face = "bold", 
+                                                    size = 20))) +
    guides(fill = guide_legend(override.aes = list(alpha = 0.3),
-                              label.theme = element_text(face = "italic", size = 10),
-                              title.theme = element_text(face = "bold", size = 10))) +
-   labs(x="", y="") +
+                              label.theme = element_text(face = "italic",
+                                                         size = 20),
+                              title.theme = element_text(face = "bold", 
+                                                         size = 20))) +
+   labs(x=NULL, y=NULL) +
    theme_classic() +
-   theme(legend.position="right",
-         text = element_text(size = 12))
+   theme(text = element_text(size = 20),
+         strip.background = element_blank(),
+         strip.text.x = element_blank(),
+         legend.position = "bottom")
 
 
-# save figure
-ggsave("Fig1_LOC1_low.svg")
-# ggsave("Fig1_LOC9_mid.svg")
-# ggsave("Fig1_LOC12_high.svg")
+
+# arrange figures and save it
+tiff("figures/Fig1AB_map.tiff", width = 9000, height = 5500, res=300)
+ggarrange(P3, Fig1B, 
+          widths = c(3.5,1.2), labels=c("A","B"),
+          font.label = list(size=40, color="black") )
+dev.off()
