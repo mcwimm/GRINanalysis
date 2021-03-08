1ef3a268dc86c2aad2d6f142c9cb962e7d0e0ba0
marie <mcwimm@bitbucket.org>
2021-03-08 15:26:32 +0100

    Update script_Supp_Inf_Fig4.R

--- figures/SupplementaryInformationFigures/script_Supp_Inf_Fig4.R
+++ figures/SupplementaryInformationFigures/script_Supp_Inf_Fig4.R
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #### Libraries required ####
 library(gratia)
 
@@ -12,6 +13,24 @@ mydraw.gam <- function (object, parametric = TRUE, select = NULL, scales = c("fr
                                                                              "fixed"), align = "hv", axis = "lrtb", n = 100, unconditional = FALSE, 
                         overall_uncertainty = TRUE, dist = 0.1, ...) 
 {
+=======
+#### Required packages ####
+if (!require("gratia")) install.packages("gratia")
+if (!require("ggplot2")) install.packages("ggplot2")
+
+#### Required data ####
+load("./data/GAMM.Rda") 
+
+#### SI Figure 4 ####
+draw(Mod)
+
+# The modified draw.gam function
+mydraw.gam <- function (object, parametric = TRUE, select = NULL, 
+                        scales = c("free", "fixed"), 
+                        align = "hv", axis = "lrtb", n = 100, 
+                        unconditional = FALSE, 
+                        overall_uncertainty = TRUE, dist = 0.1, ...){
+>>>>>>> parent of 0d0a3f9 (Delete script_Supp_Inf_Fig4.R)
   scales <- match.arg(scales)
   S <- smooths(object)
   select <- gratia:::check_user_select_smooths(smooths = S, select = select)
@@ -73,18 +92,33 @@ mydraw.gam <- function (object, parametric = TRUE, select = NULL, scales = c("fr
 }
 
 # Example no. 1
+<<<<<<< HEAD
 #dat <- gamSim(1, n = 400, dist = "normal", scale = 2, verbose = FALSE)
 #mod <- gam(y ~ s(x0),  data = dat, method = "REML")
 p <- mydraw.gam(Mod)
 
 SuppFig3A<-p[[1]]+
+=======
+# dat <- gamSim(1, n = 400, dist = "normal", scale = 2, verbose = FALSE)
+# mod <- gam(y ~ s(x0),  data = dat, method = "REML")
+p <- mydraw.gam(Mod)
+
+SuppFig3A <- p[[1]]+
+>>>>>>> parent of 0d0a3f9 (Delete script_Supp_Inf_Fig4.R)
   labs(title = "Neighbourhood asymmetry",
                             subtitle="Grafted")+
   xlab("Neighbourhood asymmetry")+
   theme(panel.grid = element_blank(),
         panel.background = element_blank(),
+<<<<<<< HEAD
         axis.text.x = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),size=12, colour = "black" ),
         axis.text.y = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),size=12, colour = "black"),
+=======
+        axis.text.x = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),
+                                   size=12, colour = "black" ),
+        axis.text.y = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),
+                                   size=12, colour = "black"),
+>>>>>>> parent of 0d0a3f9 (Delete script_Supp_Inf_Fig4.R)
         axis.title.x = element_text(size=12, colour = "black"),
         axis.title.y = element_text(size=12, colour = "black"),
         axis.ticks.length=unit(-1.5, "mm"),
@@ -93,14 +127,25 @@ SuppFig3A<-p[[1]]+
         legend.background = element_rect(fill = NA),
         legend.key = element_rect(fill = NA, color = NA))
 
+<<<<<<< HEAD
 SuppFig3B<-p[[2]] +
+=======
+SuppFig3B <- p[[2]] +
+>>>>>>> parent of 0d0a3f9 (Delete script_Supp_Inf_Fig4.R)
   labs(title = "Neighbourhood asymmetry",
        subtitle="Non-grafted")+
   xlab("Neighbourhood asymmetry")+
   theme(panel.grid = element_blank(),
         panel.background = element_blank(),
+<<<<<<< HEAD
         axis.text.x = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),size=12, colour = "black" ),
         axis.text.y = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),size=12, colour = "black"),
+=======
+        axis.text.x = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),
+                                   size=12, colour = "black" ),
+        axis.text.y = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),
+                                   size=12, colour = "black"),
+>>>>>>> parent of 0d0a3f9 (Delete script_Supp_Inf_Fig4.R)
         axis.title.x = element_text(size=12, colour = "black"),
         axis.title.y = element_text(size=12, colour = "black"),
         axis.ticks.length=unit(-1.5, "mm"),
@@ -109,14 +154,25 @@ SuppFig3B<-p[[2]] +
         legend.background = element_rect(fill = NA),
         legend.key = element_rect(fill = NA, color = NA))
 
+<<<<<<< HEAD
 SuppFig3C<-p[[3]] +
+=======
+SuppFig3C <- p[[3]] +
+>>>>>>> parent of 0d0a3f9 (Delete script_Supp_Inf_Fig4.R)
   labs(title = "Stem diameter",
        subtitle="Grafted")+
   xlab("Stem diameter (cm)")+
   theme(panel.grid = element_blank(),
         panel.background = element_blank(),
+<<<<<<< HEAD
         axis.text.x = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),size=12, colour = "black" ),
         axis.text.y = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),size=12, colour = "black"),
+=======
+        axis.text.x = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),
+                                   size=12, colour = "black" ),
+        axis.text.y = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),
+                                   size=12, colour = "black"),
+>>>>>>> parent of 0d0a3f9 (Delete script_Supp_Inf_Fig4.R)
         axis.title.x = element_text(size=12, colour = "black"),
         axis.title.y = element_text(size=12, colour = "black"),
         axis.ticks.length=unit(-1.5, "mm"),
@@ -125,14 +181,25 @@ SuppFig3C<-p[[3]] +
         legend.background = element_rect(fill = NA),
         legend.key = element_rect(fill = NA, color = NA))
 
+<<<<<<< HEAD
 SuppFig3D<-p[[4]] +
+=======
+SuppFig3D <- p[[4]] +
+>>>>>>> parent of 0d0a3f9 (Delete script_Supp_Inf_Fig4.R)
   labs(title = "Stem diameter",
        subtitle="Non-grafted")+
   xlab("Stem diameter (cm)")+
   theme(panel.grid = element_blank(),
         panel.background = element_blank(),
+<<<<<<< HEAD
         axis.text.x = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),size=12, colour = "black" ),
         axis.text.y = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),size=12, colour = "black"),
+=======
+        axis.text.x = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),
+                                   size=12, colour = "black" ),
+        axis.text.y = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),
+                                   size=12, colour = "black"),
+>>>>>>> parent of 0d0a3f9 (Delete script_Supp_Inf_Fig4.R)
         axis.title.x = element_text(size=12, colour = "black"),
         axis.title.y = element_text(size=12, colour = "black"),
         axis.ticks.length=unit(-1.5, "mm"),
@@ -141,14 +208,25 @@ SuppFig3D<-p[[4]] +
         legend.background = element_rect(fill = NA),
         legend.key = element_rect(fill = NA, color = NA))
 
+<<<<<<< HEAD
 SuppFig3E<-p[[5]] +
+=======
+SuppFig3E <- p[[5]] +
+>>>>>>> parent of 0d0a3f9 (Delete script_Supp_Inf_Fig4.R)
   labs(title = "Condition",
        subtitle="")+
   xlab("Condition")+
   theme(panel.grid = element_blank(),
         panel.background = element_blank(),
+<<<<<<< HEAD
         axis.text.x = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),size=12, colour = "black" ),
         axis.text.y = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),size=12, colour = "black"),
+=======
+        axis.text.x = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),
+                                   size=12, colour = "black" ),
+        axis.text.y = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),
+                                   size=12, colour = "black"),
+>>>>>>> parent of 0d0a3f9 (Delete script_Supp_Inf_Fig4.R)
         axis.title.x = element_text(size=12, colour = "black"),
         axis.title.y = element_text(size=12, colour = "black"),
         axis.ticks.length=unit(-1.5, "mm"),
@@ -157,16 +235,31 @@ SuppFig3E<-p[[5]] +
         legend.background = element_rect(fill = NA),
         legend.key = element_rect(fill = NA, color = NA))
 
+<<<<<<< HEAD
 SuppFig3F<-p[[7]] +
+=======
+SuppFig3F <- p[[7]] +
+>>>>>>> parent of 0d0a3f9 (Delete script_Supp_Inf_Fig4.R)
   labs(title = "Salinity",
        subtitle="")+
   xlab("Salinity")+
   ylab("Partial effect of Salinity")+
+<<<<<<< HEAD
   scale_x_discrete(labels = c("39.7", "41.6", "45.0", "45.3", "46.9", "56.1", "58.1", "58.6"))+
   theme(panel.grid = element_blank(),
         panel.background = element_blank(),
         axis.text.x = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),size=12, colour = "black" ),
         axis.text.y = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),size=12, colour = "black"),
+=======
+  scale_x_discrete(labels = c("39.7", "41.6", "45.0", "45.3", "46.9",
+                              "56.1", "58.1", "58.6"))+
+  theme(panel.grid = element_blank(),
+        panel.background = element_blank(),
+        axis.text.x = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),
+                                   size=12, colour = "black" ),
+        axis.text.y = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"),
+                                   size=12, colour = "black"),
+>>>>>>> parent of 0d0a3f9 (Delete script_Supp_Inf_Fig4.R)
         axis.title.x = element_text(size=12, colour = "black"),
         axis.title.y = element_text(size=12, colour = "black"),
         axis.ticks.length=unit(-1.5, "mm"),
@@ -176,6 +269,7 @@ SuppFig3F<-p[[7]] +
         legend.key = element_rect(fill = NA, color = NA))
 
 
+<<<<<<< HEAD
 Supp_Fig_Fig4<-ggarrange(SuppFig3A,SuppFig3B,SuppFig3C,SuppFig3D,SuppFig3E,SuppFig3F, nrow = 2,ncol=3, labels = c("a)","b)","c)","d)","e)","f)"))
 
 #### Save Figure ####
@@ -185,3 +279,15 @@ annotate_figure(Supp_Fig_Fig4,
                 top = text_grob(paste0("Supplementary Figure 4. Generalized additive mixed effects model\nshowing the effects of smooth terms on tree height\n"),  color = "black", face = "bold", size = 12, hjust = 0, x=0.01,just="left"))
 dev.off()
 
+=======
+#### Merge figures ####
+Supp_Fig_Fig4 <-ggarrange(SuppFig3A, SuppFig3B, SuppFig3C,
+                          SuppFig3D, SuppFig3E, SuppFig3F, 
+                          nrow = 2, ncol = 3, align = "hv",
+                          labels = c("a)","b)","c)","d)","e)","f)"))
+
+#### Save file ####
+tiff("figures/Sup_Info_Figure4.tiff", height = 4000, width = 7000, res=600)
+Supp_Fig_Fig4
+dev.off()
+>>>>>>> parent of 0d0a3f9 (Delete script_Supp_Inf_Fig4.R)
